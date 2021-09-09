aba54e611870a0b34f684ab577d4ef65e647bb5a
Jooah Seo <seo.jooah@gmail.com>
2021-09-09 11:53:16 -0400

    coinflip app complete

--- src/CoinFlip.js
+++ src/CoinFlip.js
@@ -6,17 +6,50 @@ import Coin from "./Coin";
 
 function CoinFlip(props) {
   const [coinSide, setCoinSide] = useState(head);
+  const [countFlip, setCountFlip] = useState(0);
+  const [headCount, setHeadCount] = useState(0);
+  const [tailCount, setTailCount] = useState(0);
+  
   const flipCoin = () => {
-    const side = coinSide === head ? tail : head;
+    const random = Math.random();
+    const side = random < 0.50 ? head : tail;
     setCoinSide(side);
+
+    updateCount();
   }
+
+  const updateCount = () => {
+    setCountFlip(countFlip + 1);
+    if(countFlip >= 10){
+      setCountFlip(0);
+      setHeadCount(0);
+      setTailCount(0);
+    }else{
+      sideCount();
+    }
+  }
+  const sideCount = () => {
+    if(coinSide === head){
+      setHeadCount(headCount +1);
+    }
+    if(coinSide === tail){
+      setTailCount(tailCount +1);
+    }
+  }
+
   return (
     <div className="app">
       <h2>Let's flip a coin!</h2>
+      {countFlip > 0 && countFlip <= 10 && 
       <Coin
       src={coinSide}
-      />
-      <button onClick={flipCoin} className="btn">Flip me</button>
+      />}
+      
+      <button onClick={flipCoin} className="btn">
+        Flip me
+        </button>
+      <p>Out of {countFlip} flips, 
+      there have been {headCount} heads and {tailCount} tails.</p>
     </div>
   );
 }
